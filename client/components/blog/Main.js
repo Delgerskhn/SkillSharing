1c0e009d7b83c6cb6da60e179ad93b52aa5d82f4
Delgerskhn <57222574+Delgerskhn@users.noreply.github.com>
2021-05-14 11:08:51 +0800

    applied slate

--- client/components/blog/Main.js
+++ client/components/blog/Main.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { createEditor } from 'slate'
+
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
+import { Slate, Editable, withReact } from 'slate-react'
 
 const useStyles = makeStyles((theme) => ({
     markdown: {
@@ -11,10 +14,15 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(3, 0),
     },
 }));
-
+const initialValue = {
+    type: "title",
+    children: [{ text: "Write..." }]
+}
 export default function Main(props) {
     const classes = useStyles();
     const { posts, title } = props;
+    const editor = useMemo(() => withReact(createEditor()), [])
+    const [value, setValue] = useState([initialValue])
 
     return (
         <Grid item xs={12} md={8}>
@@ -22,6 +30,18 @@ export default function Main(props) {
                 {title}
             </Typography>
             <Divider />
+
+            <Slate
+                editor={editor}
+                value={value}
+                onChange={newValue => {
+                    console.log(newValue);
+                    setValue(newValue)
+                }
+                }
+            >
+                <Editable />
+            </Slate>
             {/*{posts.map((post) => (
                 <Markdown className={classes.markdown} key={post.substring(0, 40)}>
                     {post}
