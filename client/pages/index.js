99c01fe9ed80ea9ab52c59610b5813ab9ea6e807
Delgerskhn <57222574+Delgerskhn@users.noreply.github.com>
2021-05-15 14:46:40 +0800

    hero added

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,6 +7,8 @@ import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 import Grid from '@material-ui/core/Grid';
 import FeaturedPost from '../components/blogs/FeaturedPost';
+import { Button } from '../components/blogs/Components';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 const featuredPosts = [
@@ -27,12 +29,45 @@ const featuredPosts = [
         imageText: 'Image Text',
     },
 ];
+const useStyles = makeStyles((theme) => ({
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    }
+}));
 
 export default function Index() {
+    const classes = useStyles();
+
   return (
       <main>
 
-
+          <div className={classes.heroContent}>
+              <Container maxWidth="sm">
+                  <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                      Watchu lookinfo?
+            </Typography>
+                  <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                      Something short and leading about the collection below—its contents, the creator, etc.
+                      Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+                      entirely.
+            </Typography>
+                  <div className={classes.heroButtons}>
+                      <Grid container spacing={2} justify="center">
+                          <Grid item>
+                              <Button variant="contained" color="primary">
+                                  Main call to action
+                  </Button>
+                          </Grid>
+                          <Grid item>
+                              <Button variant="outlined" color="primary">
+                                  Secondary action
+                  </Button>
+                          </Grid>
+                      </Grid>
+                  </div>
+              </Container>
+          </div>
 
           <Grid container spacing={4}>
               {featuredPosts.map((post) => (
