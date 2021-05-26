ca7a92f94a43416d6ec7e843270478dc4b34b8c2
delgerskhn <delgersaikhan2001@gmail.com>
2021-05-26 22:29:25 +0800

    header display email if firstname not found

--- client/components/popover.js
+++ client/components/popover.js
@@ -43,7 +43,7 @@ function AccountPopover() {
                     align="left"
                     noWrap
                 >
-                    {user.firstName}
+                    {user.firstName || user.email}
                 </Typography>
             </Button>
             <Popover
